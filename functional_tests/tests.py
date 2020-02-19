9ac18313398f52a4c4ba5d72c56c7f9ab2593204
junral <junral@163.com>
2020-02-19 21:01:43 +0800

    Add gunicorn to virtualenv requirements

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -44,7 +44,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # 应用邀请她输入一个待办事项
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholder'), 
+            inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
