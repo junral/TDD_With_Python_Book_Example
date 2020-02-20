e12318988710e62f1b4346e83095f67909c80923
junral <junral@163.com>
2020-02-20 20:33:53 +0800

    Also refactor view_list_redirect for DRY. --ch11l036-7--

--- lists/views.py
+++ lists/views.py
@@ -31,4 +31,4 @@ def view_list(request, list_id):
             return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
-    return render(request, 'list.html', {'list': list_})
+    return render(request, 'list.html', {'list': list_, "error": error})
