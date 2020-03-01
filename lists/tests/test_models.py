4e7cc8b1eb007a9f0044c0deace61f8bfc9d257c
junral <junral@163.com>
2020-03-01 21:47:31 +0800

    move model tests out into two classes. --ch13l011--

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -4,13 +4,13 @@ from django.core.exceptions import ValidationError
 
 # Create your tests here.
 
-class ListAndItemModelTest(TestCase):
+class ItemModelTest(TestCase):
 
     def test_default_text(self):
         item = Item()
         self.assertEqual(item.text, '')
 
-    def est_item_is_related_to_list(self):
+    def test_item_is_related_to_list(self):
         list_ = List.objects.create()
         item = Item()
         item.list = list_
@@ -23,10 +23,6 @@ class ListAndItemModelTest(TestCase):
         with self.assertRaises(ValidationError):
             item.save()
             item.full_clean()
-
-    def test_get_absolute_url(self):
-        list_ = List.objects.create()
-        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
 
     def test_duplicate_items_are_invalid(self):
         list_ = List.objects.create()
@@ -55,3 +51,9 @@ class ListAndItemModelTest(TestCase):
     def test_string_representation(self):
         item = Item(text='some text')
         self.assertEqual(str(item), 'some text')
+
+class ListModelTest(TestCase):
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
