27c0dc1d553851ee691c804364f17b7477c8fe22
junral <junral@163.com>
2020-03-01 21:44:19 +0800

    simplify item model tests. --ch13l010--

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -6,32 +6,16 @@ from django.core.exceptions import ValidationError
 
 class ListAndItemModelTest(TestCase):
 
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, '')
 
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        save_list = List.objects.first()
-        self.assertEqual(save_list, list_)
-
-        save_items = Item.objects.all()
-        self.assertEqual(save_items.count(), 2)
-
-        first_saved_item = save_items[0]
-        second_saved_item = save_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
+    def est_item_is_related_to_list(self):
+        list_ = List.objects.create()
+        item = Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item, list_.item_set.all())
 
     def test_cannot_save_empty_list_item(self):
         list_ = List.objects.create()
