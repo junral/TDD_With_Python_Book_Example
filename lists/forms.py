3af7192f4b47a1fd8b5f36d2074e94451f5d695f
junral <junral@163.com>
2020-03-01 23:44:47 +0800

    placeholder for ExistingListItemForm --ch09l071--

--- lists/forms.py
+++ lists/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 from lists.models import Item
 
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
+
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
@@ -22,3 +24,8 @@ class ItemForm(forms.models.ModelForm):
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
+
+class ExistingListItemForm(forms.models.ModelForm):
+
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
